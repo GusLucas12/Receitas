a9488902d6e7473d6a69aff97e28fa0cf4ae9d3e
Gustavo <gustavosilveira422.gl@gmail.com>
2024-11-21 10:35:04 -0300

    header pronta

--- client/src/components/Top.js
+++ client/src/components/Top.js
@@ -1,26 +1,31 @@
 import styles from './header.module.css';
-import {Link} from 'react-router-dom';
-function Top(){
-    return(
+import { Link } from 'react-router-dom';
+function Top() {
+    return (
         <div>
-        <header>
-         <div className={styles.interface}>
-             <div className={styles.texto}>
-                <h1>Receitas++</h1>
-                 
-             </div> 
- 
-             
-             <div className={styles.botao}>
-               
-                 <p> dnv</p>
-             </div>
- 
-         </div> 
- 
- 
-     </header>  
-       </div>
+            <header>
+                <div className={styles.interface}>
+                    <div className={styles.texto}>
+                        <h1>Receitas++</h1>
+
+                    </div>
+
+
+                    <div className={styles.botao}>
+                        <a href="#suas-receitas">Suas <br></br>receitas</a>
+                        <a href="#descubra-receitas">Descubra<br></br> Receitas</a>
+                        <a href="#placeholder">Place <br></br>Holder</a>
+                        <a href="#sua-conta">Sua <br></br>Conta</a>
+                        <div className={styles.user}>
+                            <span>👤</span>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </header>
+        </div>
     )
 }
 export default Top;
