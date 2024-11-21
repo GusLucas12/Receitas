326dd7e2466471d8a94fa6eaab0efb3a2e19af07
Gustavo <gustavosilveira422.gl@gmail.com>
2024-11-21 16:49:09 -0300

    Tela principal e formulario de submissao

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -2,7 +2,7 @@ import styles from './footer.module.css';
 function Footer(){
     return(
         <div className={styles.footer}>
-            mudou
+         
         </div>
     )
 }
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,17 +1,51 @@
 import header from "../components/cabeca";
 import styles from './home.module.css';
+import React, { useState } from "react";
+
+
+
+
 function Home() {
+    const [recipe, setRecipe] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Previne o comportamento padrão de recarregar a página
+        if (recipe) {
+            alert(`Receita escolhida: ${recipe}`);
+            setRecipe(""); // Limpa o campo após o envio
+        } else {
+            alert("Por favor, insira uma receita!");
+        }
+    };
+
     return (
         <div >
             <div className={styles.main}>
-            <main >
-                Cade essa imagem
-            </main>
+                <main >
+                    <div className={styles.box}>
+                        <h1>Receitas++</h1>
+                        <h2>Descubra Receitas inovadoras e acabe com o desperdício</h2>
+
+                    </div>
+                    <div className={styles.container}>
+
+                        <form onSubmit={handleSubmit} className={styles.form}>
+                            <input
+                                type="text"
+                                placeholder="Digite o nome da receita..."
+                                value={recipe}
+                                onChange={(e) => setRecipe(e.target.value)}
+                                className={styles.input}
+                            />
+                            <button type="submit" className={styles.button}>
+                                🔍
+                            </button>
+                        </form>
+                    </div>
+                </main>
             </div>
-           
-            <div className={styles.pe}>
-                sou um footer
-            </div>
+
+
         </div>
 
     )
