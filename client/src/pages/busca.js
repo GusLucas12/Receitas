d2bc80c49f64947d9a9a457af8595bc9e5fb88d4
Gustavo <gustavosilveira422.gl@gmail.com>
2024-11-22 09:08:06 -0300

    atualizacao busca

--- client/src/pages/busca.js
+++ client/src/pages/busca.js
@@ -14,27 +14,30 @@ function Busca() {
         }
     };
     return (
+        <div>
+            <div className={styles.main}>
+                <div className={styles.container}>
 
-        <div className={styles.main}>
-            <div className={styles.container}>
+                    <form onSubmit={handleSubmit} className={styles.form}>
+                        <input
+                            type="text"
+                            placeholder="Digite o nome da receita..."
+                            value={recipe}
+                            onChange={(e) => setRecipe(e.target.value)}
+                            className={styles.input}
+                        />
+                        <button type="submit" className={styles.button}>
+                            🔍
+                        </button>
+                    </form>
+                </div>
+                <div className={styles.telaDeBusca}>
+                    Oi
+                </div>
+            </div>
 
-                <form onSubmit={handleSubmit} className={styles.form}>
-                    <input
-                        type="text"
-                        placeholder="Digite o nome da receita..."
-                        value={recipe}
-                        onChange={(e) => setRecipe(e.target.value)}
-                        className={styles.input}
-                    />
-                    <button type="submit" className={styles.button}>
-                        🔍
-                    </button>
-                </form>
-            </div>
-            <div className={styles.telaDeBusca}>
-                Oi
-            </div>
         </div>
+
     )
 }
 export default Busca;
