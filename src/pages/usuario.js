93b053bc1c02316d963a9d82f65ea4d8451f0bd1
Gustavo <gustavosilveira422.gl@gmail.com>
2025-01-07 10:45:55 -0300

    atualizacoes perfil

--- src/pages/usuario.js
+++ src/pages/usuario.js
@@ -3,7 +3,17 @@ function Usuario() {
     return (
         <div>
             <div className={styles.container}>
-                
+                <div className={styles.perfilDetails}>
+                    <div className={styles.fotoNome}>
+                        <div className={styles.foto}></div>
+                        <div className={styles.nomeBadge}>
+                            <h1>Place Holder</h1>
+                            <span className={styles.badge}>Membro</span>
+                        </div>
+                        
+                    </div>
+                    <button className={styles.editarPerfil}>✏️</button>
+                </div>
             </div>
         </div>
     )
