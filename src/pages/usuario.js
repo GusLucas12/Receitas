5ae97f1a4f6013dbcb1e72a06560ce6a1b8ffcf2
Gustavo <gustavosilveira422.gl@gmail.com>
2025-01-09 10:46:18 -0300

    usuario , sobre mim e cards

--- src/pages/usuario.js
+++ src/pages/usuario.js
@@ -1,20 +1,60 @@
 import styles from './usuario.module.css'
+import Card from '../components/card';
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+
 function Usuario() {
+    const [cards, setCards] = useState([]);
+
+    const addCard = () => {
+        const newCard = { id: cards.length + 1, title: `Card ${cards.length + 1}` };
+        setCards([...cards, newCard]);
+    };
+
+    const deleteCard = (id) => {
+        setCards(cards.filter((card) => card.id !== id));
+    };
     return (
         <div>
             <div className={styles.container}>
-                <div className={styles.perfilDetails}>
-                    <div className={styles.fotoNome}>
-                        <div className={styles.foto}></div>
-                        <div className={styles.nomeBadge}>
-                            <h1>Place Holder</h1>
-                            <span className={styles.badge}>Membro</span>
+                <div className={styles.perfilContainer}>
+                    <div className={styles.perfilDetails}>
+                        <div className={styles.fotoNome}>
+                            <div className={styles.foto}></div>
+                            <div className={styles.nomeBadge}>
+                                <h1>Place Holder</h1>
+                                <span className={styles.badge}>Membro</span>
+                            </div>
+
                         </div>
-                        
+
+
+                        <button className={styles.editarPerfil}>✏️</button>
                     </div>
-                    <button className={styles.editarPerfil}>✏️</button>
+                    <div className={styles.descricao}>
+                        <h3>Sobre mim:</h3>
+                        <p>Gosto de Cozinhar bolos e massas</p>
+                    </div>
+                    <h1>Receitas Favoritas: </h1>
+                </div>
+                <div className={styles.receitasContainer}>
+                    <div className={styles.scroll}>
+                        {cards.map((card) => (
+                            <Card
+                                key={card.id}
+                                title={card.title}
+                                onDelete={() => deleteCard(card.id)}
+                            />
+                        ))}
+                        <div className={styles.cardAdd} onClick={addCard}>
+                            <Link><h2>+</h2></Link>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
+
         </div>
     )
 }
