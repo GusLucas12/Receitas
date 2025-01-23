a42ba4f9b866f2bf0619f0bc309942d301fdef64
Gustavo <gustavosilveira422.gl@gmail.com>
2025-01-23 09:36:59 -0300

    atualizacoes header

--- src/components/cabeca.js
+++ src/components/cabeca.js
@@ -15,8 +15,8 @@ function Top() {
                     <div className={styles.botao}>
                         <Link to='/receitas' className={styles.custom_link}><h1>Suas <br></br>receitas</h1></Link> 
                         <Link to='/busca' className={styles.custom_link}><h1>Descubra<br></br> Receitas</h1></Link>
-
-                        <h1>Place <br></br>Holder</h1>
+                        <Link to='/criar' className={styles.custom_link}><h1>Crie<br></br>Receitas</h1></Link>
+                        
                         <Link to='/usuario' className={styles.custom_link}> <h1>Sua <br></br>Conta</h1> </Link> 
                         <div className={styles.user}> 
                           <Link to='/usuario' className={styles.custom_link}> <span>👤</span> </Link> 
