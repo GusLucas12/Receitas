bf7d4aba94ac11ac94cecc486dd1805c25e0fc35
Gustavo Lucas <gustavosilveira422.gl@gmail.com>
2025-02-10 19:15:40 -0300

    loading e erro

---
+++ src/components/feedback.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './feedback.module.css';
+const FeedbackMessage = ({ messages, type, loading }) => {
+    if (loading) {
+      return (
+        <div className={`${styles.feedbackContainer} ${styles.loading}`}>
+          <p className={styles.feedbackMessage}>Carregando...</p>
+        </div>
+      );
+    }
+  
+    if (!messages || messages.length === 0) return null;
+  
+    return (
+      <div className={`${styles.feedbackContainer} ${type === "success" ? styles.success : styles.error}`}>
+        {messages.map((msg, index) => (
+          <p key={index} className={styles.feedbackMessage}>
+            {msg}
+          </p>
+        ))}
+      </div>
+    );
+  };
+  
+  export default FeedbackMessage;
