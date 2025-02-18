a1e7ebda6499ac0f16b204b64ba0c3788f1059e0
Wilker Ribeiro Guimarães <wilkerrguimaraes@hotmail.com>
2025-02-18 11:16:21 -0300

    att

--- src/pages/cadastrar.js
+++ src/pages/cadastrar.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./login.module.css";
+import styles from "./cadastrar.module.css";
 import InputField from "../components/input";
 import FeedbackMessage from "../components/feedback";
 
